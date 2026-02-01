7714c3b2b95241a792c52cff13e14aa788dceee4
DEVESH KUMAR VERMA <69028322+TPF-DEVESH@users.noreply.github.com>
2026-02-01 15:43:40 +0530

    Update signup.js

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,10 +1,23 @@
 export default function Signup() {
+  const submit = async () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    await fetch("http://localhost:5000/api/auth/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+    });
+
+    window.location.href = "/login";
+  };
+
   return (
     <div style={{ padding: 60 }}>
       <h2>Create Account</h2>
-      <input placeholder="Email" /><br /><br />
-      <input placeholder="Password" type="password" /><br /><br />
-      <button>Sign Up</button>
+      <input id="email" placeholder="Email" /><br /><br />
+      <input id="password" type="password" /><br /><br />
+      <button onClick={submit}>Sign Up</button>
     </div>
   );
 }
