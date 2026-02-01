10027571ad2e9b0badeabad1344785c4d6cbd2ae
DEVESH KUMAR VERMA <69028322+TPF-DEVESH@users.noreply.github.com>
2026-02-01 18:59:56 +0530

    Update pricing.js

--- frontend/pages/pricing.js
+++ frontend/pages/pricing.js
@@ -1,6 +1,6 @@
 import { getToken } from "../utils/auth";
 
-const API = "http://localhost:5000";
+const API = "https://certifynow-siyv.onrender.com";
 
 export default function Pricing() {
   const buy = async (plan) => {
