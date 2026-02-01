24e4e5eafbbdff722e909a71045bb0588e6dc33f
DEVESH KUMAR VERMA <69028322+TPF-DEVESH@users.noreply.github.com>
2026-02-01 15:27:58 +0530

    Create signup.js

---
+++ frontend/pages/signup.js
@@ -0,0 +1,10 @@
+export default function Signup() {
+  return (
+    <div style={{ padding: 60 }}>
+      <h2>Create Account</h2>
+      <input placeholder="Email" /><br /><br />
+      <input placeholder="Password" type="password" /><br /><br />
+      <button>Sign Up</button>
+    </div>
+  );
+}
