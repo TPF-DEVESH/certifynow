182f7f0ed3c44b07aadf8034d6a1a9885808bc25
DEVESH KUMAR VERMA <69028322+TPF-DEVESH@users.noreply.github.com>
2026-02-01 15:42:37 +0530

    Create auth.js

---
+++ frontend/utils/auth.js
@@ -0,0 +1,13 @@
+export const getToken = () => {
+  if (typeof window === "undefined") return null;
+  return localStorage.getItem("token");
+};
+
+export const setToken = (token) => {
+  localStorage.setItem("token", token);
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+  window.location.href = "/login";
+};
