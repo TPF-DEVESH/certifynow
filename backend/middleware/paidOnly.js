92f0998f569cb920761d9bf6b2d3cdb318b1f288
DEVESH KUMAR VERMA <69028322+TPF-DEVESH@users.noreply.github.com>
2026-02-04 23:35:50 +0530

    Delete backend directory

--- backend/middleware/paidOnly.js
+++
@@ -1,9 +0,0 @@
-const User = require("../models/User");
-
-module.exports = async (req, res, next) => {
-  const user = await User.findById(req.userId);
-  if (user.plan === "free") {
-    return res.status(403).send("Paid plan required");
-  }
-  next();
-};
--- backend/routes/admin.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const Log = require("../models/Log");
-
-router.get("/users", async (_, res) => res.json(await User.find()));
-router.get("/logs", async (_, res) => res.json(await Log.find()));
-
-module.exports = router;
